0e9e9acdb34a80f3dffc5bde25106f3187fc1214
Ansh Kumar <aanshkumar321@gmail.com>
2024-04-22 17:52:42 +0530

    updated

---
+++ src/main/java/com/anshcodes/productservice/PeoductModels/User.java
@@ -0,0 +1,21 @@
+package com.anshcodes.productservice.PeoductModels;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class User {
+    @Id
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+
+}
